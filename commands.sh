b8cd5fe5ea114a899ed1fcf91bbfce1d30047608
fspaolo <fspaolo@gmail.com>
2020-06-03 14:23:24 -0700

    Add shel script with all commands

---
+++ commands.sh
@@ -0,0 +1,20 @@
+pwd
+ls
+ls -1
+ls -l
+cd
+pwd
+ls
+cd -
+cd ..
+pwd
+ls
+cd -
+mkdir junk
+echo 'this text is printed on the screen'
+echo 'this text is saved to a file' > file.txt
+mv file.txt junk.txt
+mv junk.txt junk/
+cat junk/junk.txt
+rm -r junk/junk.txt
+cat commands.sh
--- test.sh
+++
@@ -1,8 +0,0 @@
-echo 'this is the content of the current folder'
-ls -l
-cat test.sh
-echo 'this is a print test'
-cd ..
-ls
-cd -
-pwd
